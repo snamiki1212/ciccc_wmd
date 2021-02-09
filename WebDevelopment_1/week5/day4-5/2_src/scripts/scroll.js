eeda461eebad6a129b9a9a5a6ed933223ba783df
snamiki1212 <snamiki1212@gmail.com>
2021-02-09 11:57:33 -0800

    Update scroll animation

--- WebDevelopment_1/week5/day4-5/2_src/scripts/scroll.js
+++ WebDevelopment_1/week5/day4-5/2_src/scripts/scroll.js
@@ -1,36 +1,49 @@
 const HIDDEN_CLASS = "hidden";
 const EASE_IN_ANIME_CLASS = "ease-in";
-const FADE_IN_DOWN_ANIME_CLASS = 'fadeInDown'
+const FADE_IN_DOWN_ANIME_CLASS = "fadeInDown";
+const TARGET_CLASS_LIST = [
+  //sections
+  ".section-aboutme",
+  ".section-contact",
+  ".section-experiences",
+  ".section-projects",
 
+  // etc
+  ".footer__message",
+];
 
+const selectors = TARGET_CLASS_LIST.join(",");
+
+const options = {
+  rootMargin: "-100px",
+};
 
 window.addEventListener("DOMContentLoaded", () => {
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        const elements = document.querySelectorAll(".footer__message");
+  const createCallback = (selector) => (entries) => {
+    entries.forEach((entry) => {
+      const elements = document.querySelectorAll(selector);
 
-        const isInViewport = entry.intersectionRatio > 0;
-        if (isInViewport) {
-          elements.forEach((elem) => {
-            elem.classList.remove(HIDDEN_CLASS);
-            elem.classList.add(FADE_IN_DOWN_ANIME_CLASS);
-          });
-        } else {
-          elements.forEach((elem) => {
-            elem.classList.add(HIDDEN_CLASS);
-            elem.classList.remove(FADE_IN_DOWN_ANIME_CLASS);
-          });
-        }
-      });
-    },
-    {
-      rootMargin: "-100px",
-    }
-  );
+      const isInViewport = entry.intersectionRatio > 0;
+      if (isInViewport) {
+        elements.forEach((elem) => {
+          elem.classList.remove(HIDDEN_CLASS);
+          elem.classList.add(FADE_IN_DOWN_ANIME_CLASS);
+        });
+      } else {
+        elements.forEach((elem) => {
+          elem.classList.add(HIDDEN_CLASS);
+          elem.classList.remove(FADE_IN_DOWN_ANIME_CLASS);
+        });
+      }
+    });
+  };
 
-  document.querySelectorAll(".footer__message").forEach((elem) => {
-    observer.observe(elem);
+  TARGET_CLASS_LIST.forEach((selector) => {
+    const callback = createCallback(selector);
+    const observer = new IntersectionObserver(callback, options);
+    document.querySelectorAll(selector).forEach((elem) => {
+      observer.observe(elem);
+    });
   });
 
   console.log("hey");
