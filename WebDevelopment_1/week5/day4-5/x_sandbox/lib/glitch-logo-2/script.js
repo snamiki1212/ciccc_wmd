3079ba6dddd753fd5cf3d1f4695927e3bcdea6b3
snamiki1212 <snamiki1212@gmail.com>
2021-02-09 16:49:09 -0800

    Update js

--- WebDevelopment_1/week5/day4-5/x_sandbox/lib/glitch-logo-2/script.js
+++ WebDevelopment_1/week5/day4-5/x_sandbox/lib/glitch-logo-2/script.js
@@ -1,21 +1,17 @@
 const MESSAGE = `Nash`;
-const CLOCK_CLASS = 'text-wrapper'
+const CLOCK_CLASS = '.text-wrapper'
 const REMOVED_CLASS = 'is-off'
-const TEXT_CLASS = "text";
+const TEXT_CLASS = ".text";
 
 window.addEventListener("DOMContentLoaded", function () {
-  setTimeout(function () {
-    const elements = document.getElementsByClassName(CLOCK_CLASS)
-    for(let i =0; i<elements.length; i++){
-      elements[i].classList.remove(REMOVED_CLASS)
-    }
-  }, 2000);
+  document.querySelectorAll(CLOCK_CLASS).forEach(element => {
+    element.classList.remove(REMOVED_CLASS)
+  })
 
   setInterval(function () {
-    let elements = document.getElementsByClassName(TEXT_CLASS);
-    for (let i = 0; i < elements.length; i++) {
-      elements[i].innerHTML = MESSAGE;
-      elements[i].setAttribute("data-time", MESSAGE);
-    }
+    document.getElementsByClassName(TEXT_CLASS).forEach(element => {
+      element.innerHTML = MESSAGE;
+      element.setAttribute("data-time", MESSAGE);
+    });
   }, 1000);
 });
